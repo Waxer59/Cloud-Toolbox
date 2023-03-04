5479f6d46a6301b90bfbe1fc1fcae58d605f20c0
Waxer59 <hnboyano@gmail.com>
2023-03-04 18:45:43 +0100

    init repo

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname
+  },
+  ignorePatterns: ['vite.config.ts'],
+  plugins: ['react', 'prettier'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/triple-slash-reference': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
